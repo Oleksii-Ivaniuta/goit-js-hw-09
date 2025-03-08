4abf5cdffe4b143b4a31123306bee9e5b4191443
Oleksii Ivaniuta <oivaniuta@gmail.com>
2025-03-08 14:27:38 +0000

    add form no styles

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,60 @@
 "use strict";
+
+const form = document.querySelector(".feedback-form");
+const formData = { email: "", message: "" };
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+const formBtn = form.children[2];
+const labelEmail = emailInput.parentNode;
+const labelMessage = messageInput.parentNode;
+formBtn.classList.add("form-btn");
+labelEmail.classList.add("form-label");
+labelMessage.classList.add("form-label");
+emailInput.classList.add("form-input");
+messageInput.classList.add("form-input");
+
+emailInput.addEventListener("focus", () => {
+    emailInput.setAttribute("placeholder", "Type area");
+})
+
+emailInput.addEventListener("blur", () => {
+    emailInput.setAttribute("placeholder", "");
+})
+
+messageInput.addEventListener("focus", () => {
+    messageInput.setAttribute("placeholder", "Type area");
+})
+
+messageInput.addEventListener("blur", () => {
+    messageInput.setAttribute("placeholder", "");
+})
+
+form.addEventListener("input", () => {
+    formData.email = emailInput.value;
+    formData.message = messageInput.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+});
+
+if (localStorage.getItem("feedback-form-state")) {
+    const savedEmail = JSON.parse(localStorage.getItem("feedback-form-state")).email;
+    const savedMessage = JSON.parse(localStorage.getItem("feedback-form-state")).message;
+    formData.email = savedEmail;
+    formData.message = savedMessage;
+    emailInput.value = savedEmail;
+    messageInput.value = savedMessage;
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (emailInput.value && messageInput.value) {
+        console.log(formData);
+        form.reset();
+        localStorage.removeItem("feedback-form-state");
+        return
+    }
+    alert(`Fill please all fields`);
+});
+
+
+
+
