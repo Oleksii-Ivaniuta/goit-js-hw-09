119eb9b509cbc1ae93449e3aa38058f98030a0b0
Oleksii Ivaniuta <oivaniuta@gmail.com>
2025-03-08 17:43:47 +0000

    hw9 ready

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,34 +1,40 @@
 "use strict";
 
+const head = document.querySelector("head");
+const monseratFontLinks = `<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>;`
+head.insertAdjacentHTML("beforeEnd", monseratFontLinks);
+
+
 const form = document.querySelector(".feedback-form");
-const formData = { email: "", message: "" };
 const emailInput = form.elements.email;
 const messageInput = form.elements.message;
 const formBtn = form.children[2];
-const labelEmail = emailInput.parentNode;
-const labelMessage = messageInput.parentNode;
+const labelEmail = emailInput.parentElement;
+const labelMessage = messageInput.parentElement;
+const formBody = form.parentElement;
+formBody.classList.add("form-body");
 formBtn.classList.add("form-btn");
 labelEmail.classList.add("form-label");
 labelMessage.classList.add("form-label");
 emailInput.classList.add("form-input");
-messageInput.classList.add("form-input");
+messageInput.classList.add("form-message", "form-input");
+
 
 emailInput.addEventListener("focus", () => {
     emailInput.setAttribute("placeholder", "Type area");
 })
-
 emailInput.addEventListener("blur", () => {
     emailInput.setAttribute("placeholder", "");
 })
-
 messageInput.addEventListener("focus", () => {
     messageInput.setAttribute("placeholder", "Type area");
 })
-
 messageInput.addEventListener("blur", () => {
     messageInput.setAttribute("placeholder", "");
 })
 
+
+const formData = { email: "", message: "" };
 form.addEventListener("input", () => {
     formData.email = emailInput.value;
     formData.message = messageInput.value;
